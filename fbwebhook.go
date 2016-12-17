57b47198415bb3557f7b555ef7f40aab5a4e90ac
Ucchishta Sivaguru <ucc.men@gmail.com>
2016-12-17 16:11:09 +0800

    handle POST callbacks

--- fbwebhook.go
+++ fbwebhook.go
@@ -1,34 +1,16 @@
 package main
 
-import (
-	"net/http"
-	"os"
-	"strings"
-)
+import "net/http"
 
 func fbWebhook(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
+	if r.Method == "GET" {
+		confirmSubscription(w, r)
+		return
+	}
+
+	// callbacks
+	if r.Method != "POST" {
 		http.Error(w, "", http.StatusMethodNotAllowed)
-		return
-	}
-
-	hubMode := strings.TrimSpace(r.FormValue("hub.mode"))
-	hubVerifyToken := strings.TrimSpace(r.FormValue("hub.verify_token"))
-	hubChallenge := strings.TrimSpace(r.FormValue("hub.challenge"))
-
-	if hubMode != "subscribe" {
-		http.Error(w, "", http.StatusExpectationFailed)
-		return
-	}
-
-	if hubVerifyToken != os.Getenv("HUB_VERIFY_TOKEN") {
-		http.Error(w, "", http.StatusForbidden)
-		return
-	}
-
-	_, err := w.Write([]byte(hubChallenge))
-	if err != nil {
-		http.Error(w, "", http.StatusInternalServerError)
 		return
 	}
 }
