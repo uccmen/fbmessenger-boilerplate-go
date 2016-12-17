4675dedb50b13cdac65ed4f94ccdaa1116657554
Ucchishta Sivaguru <ucc.men@gmail.com>
2016-12-17 20:32:28 +0800

    debug parent request

--- fbwebhook.go
+++ fbwebhook.go
@@ -1,8 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+
+	"github.com/stvp/rollbar"
+)
 
 func fbWebhook(w http.ResponseWriter, r *http.Request) {
+
+	dump, err := httputil.DumpRequestOut(r, true)
+	if err != nil {
+		rollbar.Error(rollbar.ERR, err)
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Printf("incoming request from webhook!! %q", dump)
+
 	if r.Method == "GET" {
 		confirmSubscription(w, r)
 		return
