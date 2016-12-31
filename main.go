9ab5e5eb3e965b313994bc35715116dd07942eab
Ucchishta Sivaguru <ucc.men@gmail.com>
2016-12-31 19:47:44 +0800

    test expectation failed

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "", http.StatusMethodNotAllowed)
 		return
 	}
-	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusExpectationFailed)
 	_, err := w.Write([]byte("I'm OK!"))
 	if err != nil {
 		rollbar.Error(rollbar.ERR, err)
